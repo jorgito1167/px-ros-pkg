5577ad9979ad5304cf5e65ab399dbc2d807ba80c
Jorge Perez <jorgeperez@ubuntu.(none)>
2013-06-19 16:07:41 -0400

    start integrating kalman filter

--- px_hardware_interface/px4flow_node/src/filter_node.cpp
+++ px_hardware_interface/px4flow_node/src/filter_node.cpp
@@ -1,18 +1,48 @@
 #include "ros/ros.h"
-#include "std_msgs/Float64.h"
+#include "std_msgs/Float32.h"
 #include <px_comm/OpticalFlow.h>
+#include <px4flow_node/filter.h>
 
-ros::Publisher pub;
-void kalmanFilter(px_comm::OpticalFlow msg){
-}
+using namespace MatrixWrapper;
+using namespace estimation;
+
+class NodeClass
+{
+  public:
+      
+      ros::NodeHandle n;
+      ros::Publisher filter_pub;
+      ros::Subscriber filter_sub;
+      std_msgs::Float32 newMsg;
+      BeltEstimation beltEstimator;
+      
+  //Constructor
+  NodeClass()
+  {
+      filter_pub = n.advertise<std_msgs::Float32>("ok",5);
+      filter_sub = n.subscribe("/px4flow/opt_flow",5, &NodeClass::kalmanFilter,this);
+
+  }
+  //Destructor
+  ~NodeClass()
+  {
+  }
+    void kalmanFilter(px_comm::OpticalFlow msg)
+    {
+        ColumnVector meas(2);
+        meas(0) = msg.velocity_y;
+        meas(1) = msg.velocity_x;
+        newMsg.data= msg.velocity_y;
+        filter_pub.publish(newMsg);
+    }
+};
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "filter_node");
-    ros::NodeHandle n;
-
-    ros::Subscriber sub = n.subscribe("/px4flow/opt_flow",1000, kalmanFilter);
-    
+    NodeClass nodeClass;
     ros::spin();
 
     return 0;
 }
+
